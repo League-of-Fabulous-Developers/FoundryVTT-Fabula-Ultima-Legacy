a17adf8f722689bea0fd3b6bd3cde1765626934d
spyrella <16845165+spyrella@users.noreply.github.com>
2023-07-29 20:09:46 -0400

    chore: converted #label to #name

--- module/helpers/effects.mjs
+++ module/helpers/effects.mjs
@@ -3,7 +3,7 @@
  * @param {MouseEvent} event      The left-click event on the effect control
  * @param {Actor|Item} owner      The owning document which manages this effect
  */
- export function onManageActiveEffect(event, owner) {
+export function onManageActiveEffect(event, owner) {
   event.preventDefault();
   const a = event.currentTarget;
   const li = a.closest("li");
@@ -11,7 +11,7 @@
   switch ( a.dataset.action ) {
     case "create":
       return owner.createEmbeddedDocuments("ActiveEffect", [{
-        label: "New Effect",
+        name: "New Effect",
         icon: "icons/svg/aura.svg",
         origin: owner.uuid,
         "duration.rounds": li.dataset.effectType === "temporary" ? 1 : undefined,
@@ -54,7 +54,7 @@ export function prepareActiveEffectCategories(effects) {
 
     // Iterate over active effects, classifying them into categories
     for ( let e of effects ) {
-      e._getSourceName(); // Trigger a lookup for the source name
+      // e._getSourceName(); // Trigger a lookup for the source name (deprecated in v11)
       if ( e.disabled ) categories.inactive.effects.push(e);
       else if ( e.isTemporary ) categories.temporary.effects.push(e);
       else categories.passive.effects.push(e);
